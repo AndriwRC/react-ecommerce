8969c6bff6fe0730a7df2aa38f816712fb651665
AndriwRC <randriw4@gmail.com>
2024-07-13 12:20:47 -0500

    create Navbar component with tailwind classes

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Routes from '../Routes';
+import Navbar from '../Components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,82 @@
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  const stateClassName = isActive =>
+    isActive ? 'underline underline-offset-8' : undefined;
+
+  return (
+    <nav className='w-full flex justify-between items-center fixed z-10 px-8 py-5 text-sm'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink
+            to='/'
+            className={({ isActive }) => stateClassName(isActive)}>
+            Shopi
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/all'
+            className={({ isActive }) => stateClassName(isActive)}>
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/clothes'
+            className={({ isActive }) => stateClassName(isActive)}>
+            Clothes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/electronics'
+            className={({ isActive }) => stateClassName(isActive)}>
+            Electronics
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/furniture'
+            className={({ isActive }) => stateClassName(isActive)}>
+            Furniture
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/others'
+            className={({ isActive }) => stateClassName(isActive)}>
+            Others
+          </NavLink>
+        </li>
+      </ul>
+      <ul className='flex items-center gap-3'>
+        <li className='text-black/60'>example@mail.com</li>
+        <li>
+          <NavLink
+            to='/my-orders'
+            className={({ isActive }) => stateClassName(isActive)}>
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/my-account'
+            className={({ isActive }) => stateClassName(isActive)}>
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='/sign-in'
+            className={({ isActive }) => stateClassName(isActive)}>
+            Sign In
+          </NavLink>
+        </li>
+        <li>Car</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
