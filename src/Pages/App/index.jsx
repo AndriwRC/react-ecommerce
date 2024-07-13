a04b6a12267c2c7221bdc1a3e89dc9885c495de9
AndriwRC <randriw4@gmail.com>
2024-07-12 20:18:44 -0500

    initialize project: ecommerce with ReactJS + TailwindCSS

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,22 @@
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SignIn />
+    </>
+
+  )
+}
+
+export default App;
---
+++ src/Pages/MyOrder/index.jsx
@@ -0,0 +1,8 @@
+
+function MyOrder() {
+  return (
+    <div>MyOrder</div>
+  )
+}
+
+export default MyOrder
