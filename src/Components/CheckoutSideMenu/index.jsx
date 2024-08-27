245840c7d65b8e272f99b6261155daa58a0fafed
AndriwRC <randriw4@gmail.com>
2024-08-27 16:06:41 -0500

    show products added to cart

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,16 +1,28 @@
 import { useContext } from 'react';
 import AsideMenu from '../AsideMenu';
 import { ShoppingCartContext } from '../../Context';
+import OrderCard from '../OrderCard';
 
 function CheckoutSideMenu() {
-  const { isCheckoutSideMenuOpen, closeCheckoutSideMenu } =
+  const { isCheckoutSideMenuOpen, closeCheckoutSideMenu, cartProducts } =
     useContext(ShoppingCartContext);
 
   return (
     <AsideMenu
       isOpen={isCheckoutSideMenuOpen}
       closeMenu={closeCheckoutSideMenu}
-      title={'My Order'}></AsideMenu>
+      title={'My Order'}>
+      <div className='px-6'>
+        {cartProducts.map(product => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+          />
+        ))}
+      </div>
+    </AsideMenu>
   );
 }
 
---
+++ src/Components/OrderCard/index.jsx
@@ -0,0 +1,24 @@
+import { HiX } from 'react-icons/hi';
+
+function OrderCard({ title, image, price }) {
+  return (
+    <div className='flex justify-between items-center mb-3'>
+      <div className='flex items-center gap-2'>
+        <figure className='w-20 h-20'>
+          <img
+            className='w-full h-full rounded-lg object-cover'
+            src={image}
+            alt={title}
+          />
+        </figure>
+        <p className='text-sm font-light'>{title}</p>
+      </div>
+      <div className='flex items-center gap-2'>
+        <p className='text-lg font-medium'>{price}</p>
+        <HiX className='h-6 w-6 text-black cursor-pointer' />
+      </div>
+    </div>
+  );
+}
+
+export default OrderCard;
