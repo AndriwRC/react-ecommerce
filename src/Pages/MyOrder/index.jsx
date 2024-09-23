7a00bf2aa3ae4ff7c96c08476f00d22e21c558a3
AndriwRC <randriw4@gmail.com>
2024-09-23 15:46:57 -0500

    set responsive design for orders components

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -10,13 +10,13 @@ function MyOrder() {
   const index = isNaN(orderId) ? -1 : orderId;
   return (
     <>
-      <div className='flex items-center justify-center relative w-80 mb-6'>
+      <div className='flex items-center justify-center relative w-72 sm:w-80 mb-6'>
         <Link to='/my-orders' className='absolute left-0'>
           <HiChevronLeft className='h-6 w-6 text-black cursor-pointer' />
         </Link>
         <h1 className='font-medium text-xl '>My Order</h1>
       </div>
-      <div className='flex flex-col w-80'>
+      <div className='flex flex-col w-72 sm:w-80'>
         {order.at(index)?.products.map(product => (
           <OrderCard
             key={product.id}
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -8,15 +8,17 @@ function MyOrders() {
   return (
     <>
       <h1 className='font-medium text-xl mb-6'>My Orders</h1>
-      {order.map((order, index) => (
-        <Link key={index} to={`/my-orders/${index}`}>
-          <OrdersCard
-            date={order.date}
-            totalPrice={order.totalPrice}
-            totalProducts={order.totalProducts}
-          />
-        </Link>
-      ))}
+      <div className='w-full max-w-96'>
+        {order.map((order, index) => (
+          <Link key={index} to={`/my-orders/${index}`}>
+            <OrdersCard
+              date={order.date}
+              totalPrice={order.totalPrice}
+              totalProducts={order.totalProducts}
+            />
+          </Link>
+        ))}
+      </div>
     </>
   );
 }
